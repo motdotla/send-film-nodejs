87f90abf92612818d1be2b4d53fabe7b9df41127
scottmotte <scott@scottmotte.com>
2013-08-01 17:43:12 -0700

    Swaps the code for goodfilms-graph node library

--- task.js
+++ task.js
@@ -8,42 +8,74 @@ var sendgrid_username   = process.env.SENDGRID_USERNAME;
 var sendgrid_password   = process.env.SENDGRID_PASSWORD; 
 
 var sendgrid            = require('sendgrid')(sendgrid_username, sendgrid_password);
-var request             = require('request');
+var goodfilmsGraph      = require('goodfilms-graph')();
 var _                   = require('underscore');
 
-request.get({url: 'http://goodfil.ms/graph/graph.json', json:true}, function (error, response, body) {
-  if (!error && response.statusCode == 200) {
-    var filtered_points = _.filter(body.points, function(point) { return point.x >= 4; });
-    var random          = _.random(0, filtered_points.length-1);
- 
-    var film            = filtered_points[random];
+var emailFilm = function(film) {
+  var file            = {
+    cid           : 'film_poster',
+    filename      : 'poster.png',
+    url           : film.image
+  };
 
-    var file            = {
-      cid           : 'film_poster',
-      filename      : 'poster.png',
-      url           : film.image
-    };
+  var html            = "<p><img src='cid:film_poster'></p>" +
+                        "<h2>"+film.title+"</h2>" +
+                        "<p><a href='http://goodfil.ms"+film.url+"'>"+film.url+"</a></p>"; 
 
-    var html            = "<p><img src='cid:film_poster'></p>" +
-                          "<h2>"+film.title+"</h2>" +
-                          "<p><a href='http://goodfil.ms"+film.url+"'>"+film.url+"</a></p>" +
-                          "<p><a href=''></a>";
-    
-    var payload     = {
-      to          : [to, 'scott@scottmotte.com'], 
-      from        : to, 
-      subject     : '[send-film] delivery',
-      files       : [file],
-      html        : html 
+  var additional_html = "";
+  if (film.netflix_url) {
+    additional_html   += "<p><a href='"+film.netflix_url+"'>Netflix</a></p>";
+  }
+  if (film.itunes_url) {
+    additional_html   += "<p><a href='"+film.itunes_url+"'>iTunes</a></p>";
+  }
+  if (film.amazon_url) {
+    additional_html   += "<p><a href='"+film.amazon_url+"'>Amazon</a></p>";
+  }
+  if (film.hulu_url) {
+    additional_html   += "<p><a href='"+film.hulu_url+"'>Hulu</a></p>";
+  }
+
+  html += additional_html;
+  
+  var payload     = {
+    to          : to, 
+    from        : to, 
+    subject     : '[send-film] delivery',
+    files       : [file],
+    html        : html 
+  }
+
+  sendgrid.send(payload, function(err, json) {
+    if (err) { 
+      console.error(err); 
+      return process.exit();
     }
 
-    sendgrid.send(payload, function(err, json) {
-      if (err) { return console.error(err); }
-      console.log("Email sent with content: "+json);
+    console.log("Email sent with content: "+JSON.stringify(json));
 
-      process.exit();
-    });
-  } else {
-    process.exit();
-  }
-});
+    return process.exit();
+  });
+
+};
+
+var chooseRandomFilm = function() {
+  console.log('Attempting to choose random film');
+
+  goodfilmsGraph.random({x: 4, y: 4}, function(err, film) {
+    console.log(film);
+
+    if (err) { 
+      console.error(err); 
+      return process.exit();
+    }
+
+    if (film.netflix_url || film.itunes_url || film.amazon_url || film.hulu_url) {
+      return emailFilm(film);
+    } else {
+      chooseRandomFilm();
+    } 
+  });
+};
+
+chooseRandomFilm();
